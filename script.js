16fa499e0687ecf7d84a336466a5e0cb97a201bd
Revi-Universe <dldnwn99@gmail.com>
2023-02-14 06:11:04 +0900

    파이어베이스에 업로드하기

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import { getStorage, ref, uploadBytes } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-storage.js';
+
+const storage = getStorage();
+
 const dropHere = document.getElementById('dropHere');
 const dropFile = document.getElementById('dropFile');
 
@@ -19,6 +23,11 @@ function dragOff(event) {
 function dragFile(event) {
     dragOff(event);
     var file = event.dataTransfer.files;
+    fileUpload(file[0]);
+}
+
+async function fileUpload(file) {
+    return await uploadBytes(ref(storage, '/waangImages/' + file.name), file)
 }
 
 dropHere.addEventListener('dragenter', dragOn);
@@ -28,3 +37,7 @@ dropHere.addEventListener('drop', dragFile)
 dropHere.addEventListener('dragleave', dragOff)
 
 document.body.addEventListener('dragover', preventDefaults);
+
+dropFile.addEventListener('change', e => {
+    fileUpload(dropFile.files[0]);
+});
